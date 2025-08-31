917b219f0fec577dd43e3b1e90ddf21621cccff9
Debjyoti Mohapatra <debjyoti@reachmobile.com>
2025-08-31 12:56:21 +0530

    Add GitHub Actions workflow for deploying Svelte app to GitHub Pages
    - Introduced a new GitHub Actions workflow for building and deploying the Svelte application
    - Configured steps for checking out code, setting up Node.js, caching dependencies, and building the project
    - Added environment variable for GitHub Pages to ensure correct base path configuration
    - Updated Vite configuration to support deployment on GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	base: process.env.GITHUB_PAGES ? '/portfolio-website/' : '/', // Add this line
 	plugins: [
 		sveltekit(),
 		devtoolsJson(),
